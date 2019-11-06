e97f588ed931beb0b682ce3e26ad3fe45b97612c
kkpoon <kkpoon@eml.cc>
2019-11-06 22:14:41 +0800

    Move .config files into .config

--- fish_setup.sh
+++ fish_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 curl -L https://get.oh-my.fish | fish
 
-omf install krisleech
+omf install bobthefish
--- zsh/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mkdir ~/.antigen
-cd ~/.antigen
-git clone https://github.com/zsh-users/antigen.git
